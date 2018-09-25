f99e424cd6851fab8fd240d75571012ffaf6e556
Guilherme rodrigues <jogos.coisas@gmail.com>
2018-09-25 00:37:05 -0300

    primeiro commit

---
+++ Relatorios/Areas/Teste/Controllers/TesteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Relatorios.Areas.Teste.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        [Area("Teste")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
