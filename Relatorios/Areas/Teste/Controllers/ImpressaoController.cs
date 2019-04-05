f8d72b97400786ec4e965262a4f67522c22e1a86
Guilherme rodrigues <jogos.coisas@gmail.com>
2019-04-05 00:58:49 -0300

    implementado shadow dom com outro shadow dom dentro

---
+++ Relatorios/Areas/Teste/Controllers/ImpressaoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Relatorios.Areas.Teste.Controllers
+{
+    public class ImpressaoController : Controller
+    {
+        [Area("Teste")]
+        public ActionResult Index()
+        {
+            ViewBag.Impressoes = new[] { "123", "1111" };
+            return View();
+        }
+    }
+}
